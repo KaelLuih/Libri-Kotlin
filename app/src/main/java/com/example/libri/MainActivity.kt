fc43d4d4e52dc1ba18c913b52325317fe1288495
Lis <elis_jasper@estudante.sesisenai.org.br>
2026-02-18 18:36:24 -0300

    ajustes lib.versions

--- app/src/main/java/com/example/libri/MainActivity.kt
+++ app/src/main/java/com/example/libri/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.libri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.collection.mutableObjectIntMapOf
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.libri.ui.theme.LibriTheme
 
@@ -17,8 +20,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LibriTheme {
-
+                Feed()
             }
         }
+    }
+}
+
+@Composable
+fun Feed(){
+    Surface(
+        modifier = Modifier.fillMaxSize().safeDrawingPadding(),
+        color = Color(0xFFFFFFFF)
+    ) {
+
     }
 }
